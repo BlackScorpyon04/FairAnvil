fe2aa57b6b0af509cb4dd612d4bf3b65125aadb4
Jordan Mason <jordan.mason04@outlook.com>
2021-11-27 19:43:01 +1100

    Made it so enchanting table uses xp points and not levels Fixed being able to use anvil without the required levels

--- src/main/java/com/scorp/fairanvil/FairAnvil.java
+++ src/main/java/com/scorp/fairanvil/FairAnvil.java
@@ -1,22 +1,15 @@
 package com.scorp.fairanvil;
 
-import org.bukkit.Material;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.HumanEntity;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.enchantment.EnchantItemEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.inventory.PrepareAnvilEvent;
 import org.bukkit.inventory.AnvilInventory;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.InventoryView;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.inventory.meta.Repairable;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class FairAnvil extends JavaPlugin implements Listener {
@@ -31,21 +24,38 @@ public final class FairAnvil extends JavaPlugin implements Listener {
     }
 
     @EventHandler
-    public void InventoryClick (InventoryClickEvent e) {
+    public void EnchantTable(EnchantItemEvent e) {
+        Player player = e.getEnchanter();
+        int levelCost = 1 + e.whichButton();
+        player.giveExpLevels(levelCost);
+        Experience.changeExp(player, -Experience.getExpFromLevel(levelCost));
+    }
+
+    @EventHandler
+    public void InventoryClick(InventoryClickEvent e) {
         if (e.getWhoClicked() instanceof Player){
             if (toggled){
                 Player player = (Player) e.getWhoClicked();
-
                 if (e.getInventory().getType() == InventoryType.ANVIL){
                     AnvilInventory anvil = (AnvilInventory) e.getInventory();
                     InventoryType.SlotType slotType = e.getSlotType();
                     if (slotType == InventoryType.SlotType.RESULT){
-                        player.setLevel(player.getLevel() + anvil.getRepairCost());
-                        Experience.changeExp(player, -Experience.getExpFromLevel(anvil.getRepairCost()));
+                        if (anvil.getRepairCost() <= player.getLevel() || player.getGameMode() == GameMode.CREATIVE){
+                            player.setLevel(player.getLevel() + anvil.getRepairCost());
+                            Experience.changeExp(player, -Experience.getExpFromLevel(anvil.getRepairCost()));
+                        }
                     }
                 }
             }
         }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void PrepareAnvil(PrepareAnvilEvent e) {
+        Player p = (Player) e.getViewers().get(0);
+        AnvilInventory inv = e.getInventory();
+
+        inv.setMaximumRepairCost(Integer.MAX_VALUE);
     }
 
 
